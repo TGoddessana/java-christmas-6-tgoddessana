7266dfcb64f44b0e83a8f68ffe5900acddc9cb8f
tgoddessana <twicegoddessana1229@gmail.com>
2023-11-13 02:54:23 +0900

    refactor: EventPlannerController.java -> ConsoleController.java 클래스명 변경

--- src/main/java/christmas/controller/ConsoleController.java
+++ src/main/java/christmas/controller/ConsoleController.java
@@ -1,16 +1,16 @@
 package christmas.controller;
 
+import christmas.view.WelcomeView;
+import christmas.view.console.ConsoleWelcomeView;
+
 public class ConsoleController {
+    private static final WelcomeView welcomeView = new ConsoleWelcomeView();
+
     public void run() {
-        ConsoleInputView consoleInputView = new ConsoleInputView();
-        ConsoleOutputView consoleOutputView = new ConsoleOutputView();
+        welcome();
+    }
 
-        // 환영 메시지 출력
-        consoleOutputView.displayWelcomeMessage();
-
-        // 방문 날짜 입력
-        Integer visitDate = consoleInputView.getVisitDateInput();
-
-        // 주문할 메뉴, 개수 입력
+    public void welcome() {
+        welcomeView.displayWelcomeMessage();
     }
 }
