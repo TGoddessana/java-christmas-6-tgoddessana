5f04f9d1383782918ddbe72b557b55c897f17c6d
tgoddessana <twicegoddessana1229@gmail.com>
2023-11-13 23:49:20 +0900

    fix: 요구사항에 맞게 에러 메시지 수정

--- src/main/java/christmas/view/console/ConsoleVisitDateView.java
+++ src/main/java/christmas/view/console/ConsoleVisitDateView.java
@@ -4,7 +4,7 @@ import christmas.view.VisitDateView;
 
 public class ConsoleVisitDateView extends ConsoleView implements VisitDateView {
     private static final String ASK_VISIT_DATE_MESSAGE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
-    private static final String INVALID_INPUT_MESSAGE = "숫자만 입력해 주세요.";
+    private static final String INVALID_INPUT_MESSAGE = "유효하지 않은 날짜입니다. 다시 입력해 주세요.";
 
     @Override
     public Integer inputVisitDate() {
