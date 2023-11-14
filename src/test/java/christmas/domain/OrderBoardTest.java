6c6334de6d88cce313e4e992c7fd5f4c7c8c3601
tgoddessana <twicegoddessana1229@gmail.com>
2023-11-14 23:51:42 +0900

    test: 주문 메뉴가 20개 이상인 경우에 대한 예외 처리를 OrderBoard 객체가 수행하도록 강제하는 테스트 코드 작성

---
+++ src/test/java/christmas/domain/OrderBoardTest.java
@@ -0,0 +1,34 @@
+package christmas.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class OrderBoardTest {
+
+    @DisplayName("주문 메뉴가 20개 이하일 경우, 예외를 발생시키지 않는다.")
+    @Test
+    public void testValidOrderBoardCreation() {
+        List<OrderItem> validOrderItems = Arrays.asList(new OrderItem("Item1", 5), new OrderItem("Item2", 10),
+                new OrderItem("Item3", 5));
+
+        OrderBoard orderBoard = new OrderBoard(validOrderItems);
+
+        assertThat(orderBoard).isNotNull();
+        assertThat(orderBoard.toString()).contains("Item1", "Item2", "Item3");
+    }
+
+    @DisplayName("주문 메뉴가 20개를 초과할 경우, 예외를 발생시킨다.")
+    @Test
+    public void testOrderBoardWithExceedingTotalQuantity() {
+        List<OrderItem> orderItemsExceedingTotal = Arrays.asList(new OrderItem("Item1", 19),
+                new OrderItem("Item2", 19));
+
+        assertThatThrownBy(() -> new OrderBoard(orderItemsExceedingTotal)).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("총 수량은 20개 이하여야 합니다.");
+    }
+}
