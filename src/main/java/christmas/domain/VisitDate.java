d36ab47a9d9296909992075de1b36b5a1fbf3e9e
tgoddessana <twicegoddessana1229@gmail.com>
2023-11-15 01:26:04 +0900

    feat: 방문 날짜의 요일, 주말 여부 계산 기능 구현

--- src/main/java/christmas/domain/VisitDate.java
+++ src/main/java/christmas/domain/VisitDate.java
@@ -1,16 +1,28 @@
 package christmas.domain;
 
-public class VisitDate {
-    private final int date;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
 
-    public VisitDate(Integer date) {
-        validate(date);
-        this.date = date;
+public class VisitDate {
+    private final LocalDate date;
+
+    public VisitDate(int dayOfMonth) {
+        validate(dayOfMonth);
+        this.date = LocalDate.of(2023, 12, dayOfMonth);
     }
 
-    private void validate(Integer date) {
-        if (date < 1 || date > 31) {
+    private void validate(int dayOfMonth) {
+        if (dayOfMonth < 1 || dayOfMonth > 31) {
             throw new IllegalArgumentException("유효하지 않은 날짜입니다. 다시 입력해 주세요.");
         }
+    }
+
+    public DayOfWeek getDayOfWeek() {
+        return date.getDayOfWeek();
+    }
+
+    public boolean isWeekend() {
+        DayOfWeek dayOfWeek = getDayOfWeek();
+        return dayOfWeek == DayOfWeek.FRIDAY || dayOfWeek == DayOfWeek.SATURDAY;
     }
 }
