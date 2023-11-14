2e325706947b181ca9423ea5cb84757c1a40763c
tgoddessana <twicegoddessana1229@gmail.com>
2023-11-14 22:47:43 +0900

    refactor: 식당 오픈 전 메뉴판을 준비하는 역할을 MenuService 의 역할로 위임

--- src/main/java/christmas/controller/ConsoleController.java
+++ src/main/java/christmas/controller/ConsoleController.java
@@ -1,9 +1,8 @@
 package christmas.controller;
 
 import christmas.domain.MenuBoard;
-import christmas.domain.MenuCategory;
-import christmas.domain.MenuItem;
 import christmas.domain.VisitDate;
+import christmas.service.MenuService;
 import christmas.view.ErrorView;
 import christmas.view.OrderView;
 import christmas.view.VisitDateView;
@@ -12,7 +11,7 @@ import christmas.view.console.ConsoleErrorView;
 import christmas.view.console.ConsoleOrderView;
 import christmas.view.console.ConsoleVisitDateView;
 import christmas.view.console.ConsoleWelcomeView;
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class ConsoleController {
@@ -21,11 +20,23 @@ public class ConsoleController {
     private static final OrderView orderView = new ConsoleOrderView();
     private static final ErrorView errorView = new ConsoleErrorView();
 
+    private static final MenuService menuService = new MenuService();
+
     public void run() {
-        setUpMenuBoard();
+        // 식당 오픈 전, 메뉴판을 미리 만들어둡니다.
+        MenuBoard menuBoard = menuService.setUpMenuBoard();
+
+        // 손님에게 환영 메시지를 보여줍니다.
         welcome();
-        getVisitDate();
+
+        // 손님에게 방문할 날짜를 입력받습니다.
+        VisitDate visitDate = getVisitDate();
+
+        // 손님에게 주문을 입력받습니다.
         getOrder();
+
+        // 손님에게 주문 내역을 보여줍니다.
+
     }
 
     private void welcome() {
@@ -42,55 +53,13 @@ public class ConsoleController {
         }
     }
 
-    private void getOrder() {
+    private List<HashMap<String, Integer>> getOrder() {
         try {
-            orderView.inputOrder();
+            return orderView.inputOrder();
+
         } catch (IllegalArgumentException e) {
             errorView.displayErrorMessage(e.getMessage());
-            getOrder();
+            return getOrder();
         }
-    }
-
-    private MenuBoard setUpMenuBoard() {
-        List<MenuCategory> menuCategories = new ArrayList<>();
-
-        menuCategories.add(cookAppetizer());
-        menuCategories.add(cookMain());
-        menuCategories.add(cookDessert());
-        menuCategories.add(cookDrink());
-
-        return new MenuBoard(menuCategories);
-    }
-
-    private MenuCategory cookAppetizer() {
-        MenuCategory appetizer = new MenuCategory("애피타이저");
-        appetizer.addMenu(new MenuItem("양송이수프", 6000, appetizer));
-        appetizer.addMenu(new MenuItem("타파스", 5500, appetizer));
-        appetizer.addMenu(new MenuItem("시저샐러드", 8000, appetizer));
-        return appetizer;
-    }
-
-    private MenuCategory cookMain() {
-        MenuCategory main = new MenuCategory("메인");
-        main.addMenu(new MenuItem("티본스테이크", 55000, main));
-        main.addMenu(new MenuItem("바비큐립", 54000, main));
-        main.addMenu(new MenuItem("해산물파스타", 35000, main));
-        main.addMenu(new MenuItem("크리스마스파스타", 25000, main));
-        return main;
-    }
-
-    private MenuCategory cookDessert() {
-        MenuCategory dessert = new MenuCategory("디저트");
-        dessert.addMenu(new MenuItem("초코케이크", 15000, dessert));
-        dessert.addMenu(new MenuItem("아이스크림", 5000, dessert));
-        return dessert;
-    }
-
-    private MenuCategory cookDrink() {
-        MenuCategory drink = new MenuCategory("음료");
-        drink.addMenu(new MenuItem("제로콜라", 3000, drink));
-        drink.addMenu(new MenuItem("레드와인", 60000, drink));
-        drink.addMenu(new MenuItem("샴페인", 25000, drink));
-        return drink;
     }
 }
